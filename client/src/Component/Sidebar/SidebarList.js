cc7f992ca7e1c845cf77ad33e5cdb7493e168d00
Thilagavijayan <thilagavathiv32@gmail.com>
2022-11-11 23:02:13 +0530

    Updating Icons

--- client/src/Component/Sidebar/SidebarList.js
+++ client/src/Component/Sidebar/SidebarList.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import "./SidebarList.css";
 import profilepic from "../../image/image.jpeg"
-import { FcHome} from "react-icons/fc";
+import 
+{   FcHome ,
+    FcBusinesswoman,
+    FcTodoList,
+    FcTimeline ,
+    FcDiploma1,
+    FcFactory,
+    FcMultipleDevices,
+    FcDiploma2,
+    FcBusinessContact
+} from "react-icons/fc";
 
 
 const SidebarList = ({expandSidebar}) => {
@@ -14,16 +24,78 @@ const SidebarList = ({expandSidebar}) => {
         <img src={profilepic} alt="profilepicture"/>
         </div>
         
+        
         <ul>
-            <li className='nav-item'>Home</li>
+        
+            <li className='nav-item'>
+                {" "}
+                <FcHome size={25}/> Home{" "}
+            </li>
+            <li className='nav-item'>
+                <FcBusinesswoman size={25}/> About
+            </li>
+            <li className='nav-item'>
+                <FcTodoList size={25}/> Education
+            </li>
+            <li className='nav-item'>
+                <FcTimeline size={25}/> Projects
+            </li>
+            <li className='nav-item'>
+                <FcDiploma1 size={25}/> Certifications
+            </li>
+            <li className='nav-item'>
+                <FcFactory size={25}/> Internships
+            </li>
+            <li className='nav-item'>
+                <FcMultipleDevices size={25}/> Tech Stack
+            </li>
+            <li className='nav-item'>
+                <FcDiploma2 size={25}/> Achievements
+            </li>
+            <li className='nav-item'>
+                <FcBusinessContact size={25}/> Contact
+            </li>
+                
         </ul>
         </div>
-):(
-    <div></div>
+) : (
+    <div className='navbar-items-only-icons'>
+        <ul>
+        <li className='nav-item'>
+                <FcHome size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcBusinesswoman size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcTodoList size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcTimeline size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcDiploma1 size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcFactory size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcMultipleDevices size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcDiploma2 size={25}/> 
+            </li>
+            <li className='nav-item'>
+                <FcBusinessContact size={25}/>
+            </li>
+
+        
+        </ul>
+    </div>
 )}
         
     </React.Fragment>
-  );
+    )
 };
 
 export default SidebarList
